aefae7541eba70573e8f51c68d493868114c4664
brianferguson <brian.ferguson@mobileappscompany.us>
2019-10-01 01:08:43 -0400

    added addPerson activity:

---
+++ app/src/main/java/com/example/photoemiperson/AddPerson.kt
@@ -0,0 +1,12 @@
+package com.example.photoemiperson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPerson : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_person)
+    }
+}
--- app/src/main/java/com/example/photoemiperson/EMICalculator.kt
+++ app/src/main/java/com/example/photoemiperson/EMICalculator.kt
@@ -3,17 +3,21 @@ package com.example.photoemiperson
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_emicalulator.*
-import kotlinx.android.synthetic.main.activity_main.*
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class EMICalculator : AppCompatActivity(), View.OnClickListener {
+class EMICalculator : AppCompatActivity(), View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emicalulator)
         btnCalculate.setOnClickListener(this)
+        sbLoanAmount.setOnSeekBarChangeListener(this)
+        sbInterestRate.setOnSeekBarChangeListener(this)
+        sbTenure.setOnSeekBarChangeListener(this)
 
     }
 
@@ -23,10 +27,27 @@ class EMICalculator : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+        when(seekBar.id){
+            R.id.sbLoanAmount -> tvLoanAmount.text = progress.toString()
+            R.id.sbInterestRate -> tvInterestRate.text = progress.toString()
+            R.id.sbTenure -> tvTenure.text = progress.toString()
+
+        }
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+    }
+
     private fun calulateEMI(){
-        val loanAmount = etLoanAmount.text.toString().toDouble()
-        val intersetRate = (etIntersetRate.text.toString().toDouble() / 12) / 100
-        val tenure = etTenure.text.toString().toDouble()
+        val loanAmount = tvLoanAmount.text.toString().toDouble()
+        val intersetRate = (tvInterestRate.text.toString().toDouble() / 12) / 100
+        val tenure = tvTenure.text.toString().toDouble()
 
         val emi = BigDecimal((loanAmount) * (intersetRate) * ((Math.pow((1 + intersetRate), tenure)) /
                 ((Math.pow(1 + intersetRate, tenure)) - 1))).setScale(2, RoundingMode.CEILING)
